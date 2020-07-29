159c4cb27874818f142112e8a27626daf47971d4
Jacqueline Dean <jacquelineadean@gmail.com>
2020-07-29 12:02:26 -0500

    eslint fixes

--- models/post.js
+++ models/post.js
@@ -1,32 +1,32 @@
 // Creating the Post model
-module.exports = (sequelize, DataTypes) => {
-    const Post = sequelize.define("Post", {
-        // Activity cannot be null and must be a valid URL
-        activity: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                isURL: {
-                    msg: "Activity must be a URL with http or https protocol.",
-                    protocols: ["http", "https"],
-                    require_protocol: true
-                }
-            }
-        },
-        // Body is optional
-        body: {
-            type: DataTypes.TEXT
+module.exports = function(sequelize, DataTypes) {
+  var Post = sequelize.define("Post", {
+    // Activity cannot be null and must be a valid URL
+    activity: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isURL: {
+          msg: "Activity must be a URL with http or https protocol.",
+          protocols: ["http", "https"],
+          requireProtocol: true
         }
+      }
+    },
+    // Body is optional
+    body: {
+      type: DataTypes.TEXT
+    }
+  });
+
+  // Create association between Post and User
+  Post.associate = function(models) {
+    Post.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
     });
+  };
 
-    // Create association between Post and User
-    Post.associate = models => {
-        Post.belongsTo(models.User, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
-
-    return Post;
-}
+  return Post;
+};
