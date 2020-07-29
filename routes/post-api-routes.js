7d733d452c07aa7746b55af75f51fad999170f22
Jacqueline Dean <jacquelineadean@gmail.com>
2020-07-29 14:50:27 -0500

    Added routes for post and user

---
+++ routes/post-api-routes.js
@@ -0,0 +1,62 @@
+// *********************************************************************************
+// post-api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our models
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+  // GET route for getting all of the posts
+  app.get("/api/posts", function(req, res) {
+    // Retrieve all of the posts
+    db.Post.findAll({}).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+  // POST route for saving a new post
+  app.post("/api/posts", function(req, res) {
+    // Create a new post object
+    db.Post.create({
+      activity: req.body.activity,
+      body: req.body.body
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  // DELETE route for deleting posts
+  app.delete("/api/posts/:id", function(req, res) {
+    db.Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+  // PUT route for updating posts
+  app.put("/api/posts", function(req, res) {
+    db.Post.update(req.body, {
+      where: {
+        id: req.body.id
+      }
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+};
