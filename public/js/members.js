3ece13493ae5a328c0ee7dcd4e0e64f3b0284ccd
Steven Johnson <sjohnson21960@gmail.com>
2020-07-30 13:44:19 -0500

    API routes for calling profile table info

--- public/js/members.js
+++ public/js/members.js
@@ -2,6 +2,17 @@ $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.email);
+    $("#user-username").text(data.username);
+  });
+});
+
+$(document).ready(function() {
+  // This file just does a GET request to figure out which user is logged in
+  // and updates the HTML on the page
+  $.get("/api/profile_data").then(function(data) {
+    $("#profile-firstName").text(data.firstName);
+    $("#profile-lastName").text(data.lastName);
+    $("#profile-bio").text(data.bio);
+    $("#profile-photo").src(data.photo);
   });
 });
