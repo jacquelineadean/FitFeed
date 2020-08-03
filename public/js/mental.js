5dbf213ca5046883bb6e4cde924c3d60445aac6e
Steven Johnson <sjohnson21960@gmail.com>
2020-08-03 09:47:45 -0500

    new API key

--- public/js/mental.js
+++ public/js/mental.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   var youTubeApiPartTwo = "&type=video&safeSearch=strict&maxResults=";
   var youtubeApiPartThree = "&key=";
   var maxResults = 3;
-  var APIkey = "AIzaSyDermk6zMO6c6R1nXgYo-O5nBynlFV5TiI";
+  var APIkey = "AIzaSyA1CsSmWes8g2YuNsHprVY16NPYsf0kRgk";
 
   var youtubeFrame =
     "<div class='col video-cell position-relative'><iframe class='position-absolute' src='https://www.youtube.com/embed/{VIDEOID}'></iframe><div class='click-video position-absolute' id='{VIDEOID}' ></div></div>";
